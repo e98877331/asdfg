06a9c1c49ee827ac06d0ab0d618021f197ff4e53
ChengYan Yang <e98877331@gmail.com>
2013-02-25 18:26:17 +0800

    prototype of engine

--- MenDuel/src/ytwhyc/wcm/menduel/MainActivity.java
+++ MenDuel/src/ytwhyc/wcm/menduel/MainActivity.java
@@ -1,8 +1,12 @@
 package ytwhyc.wcm.menduel;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button startBtn = (Button)findViewById(R.id.button1);
+		startBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+			Intent intent = new Intent(MainActivity.this,FightActivity.class);
+			startActivity(intent);
+				
+			}
+		});
+		
 	}
 
 	@Override
